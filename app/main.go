e91cc1c29133bb2007ea8b02f3f6d22d43e18945
sseybold <sseybold@vertafore.com>
2025-08-20 10:00:17 -0600

    add loop, read, write

--- app/main.go
+++ app/main.go
@@ -25,7 +25,6 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	conn.Write([]byte("+PONG\r\n"))
 
 	for {
         // Block until we receive an incoming connection
